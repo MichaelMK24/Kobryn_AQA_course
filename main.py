111e73d34e6d2bb9407489681a7de9cb5200cacf
Kobryn_lesson2 <mikekobryn67@gmail.com>
2023-06-02 16:27:37 +0300

    some notes from lesson 4

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from pythonProject.something import something
+
 print("this is main file from 1-st lesson")
+
+a = 12
+x = a
+print(something(x))
--- something.py
+++ something.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 
-df = pd.DataFrame({'A': [1, 2, 4]})
-
+df = pd.DataFrame({'A': [1, 2, 4], 'B': [0, 3, 6]})
 print(df)
+
+
+def something(x: int) -> int:
+    return x ** 2
+
+
+x = 7
+print(something(x))
+
